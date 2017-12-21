6bd6e74e1987b6623780629e96bbc2d118f36243
Alex Lee <alex.hs.lee@gmail.com>
2017-12-20 23:42:57 -0500

    lock_all_except_active 	Locks all layers in Photoshop except for the one currently 	selected. Some testing done, seems to work. unlock_all_layers 	Uh yeah. It unlocks all the layers in the current photoshop 	document.

---
+++ lock_all_except_active.jsx
@@ -0,0 +1,30 @@
+﻿/* Script to lock all layers execpt the current one (to prevent accidentally painting on wrong layer) 
+    
+    Author: Alex Lee
+    Email: alex.hs.lee@gmail.com
+ */
+
+/*
+  * activeDoc - the photoshop document in which to lock all layers except active.
+  * activeLayer - the only layer which we will leave unlocked.
+  */
+function lockAllExceptActive(activeDoc, activeLayer)  {
+    var len = activeDoc.layers.length;
+    
+    for (var i = 0; i < len; i++) {
+        var layer = activeDoc.layers[i];
+        // If layer is in fact a group, recursive call
+        if (layer.typename == 'LayerSet') {
+            lockAllExceptActive(layer, activeLayer);
+         } else {
+             layer.allLocked = true;
+         }
+     }
+    
+    // Unlock active layer
+    activeLayer.allLocked = false;
+ }
+
+var activeLayer = app.activeDocument.activeLayer;
+// alert("Locking all layers except " + activeLayer.name);
+lockAllExceptActive(app.activeDocument, activeLayer);
---
+++ unlock_all_layers.jsx
@@ -0,0 +1,15 @@
+﻿var doc  = app.activeDocument;
+
+function unlockAllLayers(doc) {
+    var len = doc.layers.length;
+    for(var i = 0; i < len; i++) {
+        var layer = doc.layers[i];
+        if(layer.typename == 'LayerSet') {
+            unlockAllLayers(layer);
+        } else {
+            layer.allLocked = false;
+        }
+    }
+}
+
+unlockAllLayers (doc);
